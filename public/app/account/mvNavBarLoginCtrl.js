4d28373cae052e93a2a3dd21885f618f134b2e5c
Jawad Rashid <jawadrashid2010@gmail.com>
2014-04-22 15:07:33 +0500

    Completed Authorization

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,5 @@
 
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope,$http,mvNotifier,mvIdentity,mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope,$http,mvNotifier,mvIdentity,mvAuth,$location) {
     $scope.identity = mvIdentity;
     $scope.signin = function(username, password) {
         mvAuth.authenticateUser(username, password).then(function(success) {
@@ -9,5 +9,14 @@ angular.module('app').controller('mvNavBarLoginCtrl', function($scope,$http,mvNo
                 mvNotifier.notify("Username/Password combination incorrect");
             }
         }) ;
+    }
+
+    $scope.signout = function() {
+        mvAuth.logoutUser().then(function() {
+            $scope.username = "";
+            $scope.password = "";
+            mvNotifier.notify("You have successfully signed out!");
+            $location.path("/");
+        })
     }
 });
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"),
+    crypto = require("crypto");
 
 module.exports = function(config) {
     mongoose.connect(config.db);
@@ -13,17 +14,41 @@ module.exports = function(config) {
     var userSchema = mongoose.Schema({
         firstName: String,
         lastName: String,
-        username: String
+        username: String,
+        salt: String,
+        hashed_pwd: String,
+        roles: [String]
     });
+    userSchema.methods = {
+        authenticate: function(passwordToMatch) {
+            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+        }
+    }
 
     var User = mongoose.model("User", userSchema);
 
     User.find({}).exec(function(err, collection) {
         if(collection.length === 0) {
-            User.create({firstName: "Jawad", lastName: "Rashid", username: "jawad"}) ;
-            User.create({firstName: "Asad", lastName: "Aijaz", username: "asad"}) ;
-            User.create({firstName: "Amir", lastName: "Rashid", username: "amir"}) ;
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'jawad');
+            User.create({firstName: "Jawad", lastName: "Rashid", username: "jawad", salt: salt, hashed_pwd: hash, roles:["admin"]}) ;
+            salt = createSalt();
+            hash = hashPwd(salt, 'asad');
+            User.create({firstName: "Asad", lastName: "Aijaz", username: "asad", salt: salt, hashed_pwd: hash, roles:[]}) ;
+            salt = createSalt();
+            hash = hashPwd(salt, 'amir');
+            User.create({firstName: "Amir", lastName: "Rashid", username: "amir", salt: salt, hashed_pwd: hash}) ;
         }
     })
 
+}
+
+function createSalt()  {
+    return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+    var hmac = crypto.createHmac('sha1', salt);
+    return hmac.update(pwd).digest('hex');
 }
