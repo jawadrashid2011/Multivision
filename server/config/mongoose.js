6b23e7048dc68d7c7ba03ef4ab4c63f1ec9d05f0
Jawad Rashid <jawadrashid2010@gmail.com>
2014-04-23 17:47:17 +0500

    Validation Complete

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose"),
-    crypto = require("crypto");
+    userModel = require('../models/User');
 
 module.exports = function(config) {
     mongoose.connect(config.db);
@@ -10,45 +10,8 @@ module.exports = function(config) {
         console.log('multivision db opened');
     });
 
+    userModel.createDefaultUsers();
 
-    var userSchema = mongoose.Schema({
-        firstName: String,
-        lastName: String,
-        username: String,
-        salt: String,
-        hashed_pwd: String,
-        roles: [String]
-    });
-    userSchema.methods = {
-        authenticate: function(passwordToMatch) {
-            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-        }
-    }
-
-    var User = mongoose.model("User", userSchema);
-
-    User.find({}).exec(function(err, collection) {
-        if(collection.length === 0) {
-            var salt, hash;
-            salt = createSalt();
-            hash = hashPwd(salt, 'jawad');
-            User.create({firstName: "Jawad", lastName: "Rashid", username: "jawad", salt: salt, hashed_pwd: hash, roles:["admin"]}) ;
-            salt = createSalt();
-            hash = hashPwd(salt, 'asad');
-            User.create({firstName: "Asad", lastName: "Aijaz", username: "asad", salt: salt, hashed_pwd: hash, roles:[]}) ;
-            salt = createSalt();
-            hash = hashPwd(salt, 'amir');
-            User.create({firstName: "Amir", lastName: "Rashid", username: "amir", salt: salt, hashed_pwd: hash}) ;
-        }
-    })
 
 }
 
-function createSalt()  {
-    return crypto.randomBytes(128).toString('base64');
-}
-
-function hashPwd(salt, pwd) {
-    var hmac = crypto.createHmac('sha1', salt);
-    return hmac.update(pwd).digest('hex');
-}
